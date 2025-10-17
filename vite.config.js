110e00e1c49fc06877d844e412f65d4c0e228632
fizzajawed85 <angelfizza85@gmail.com>
2025-10-17 21:09:10 +0500

    Final Vercel setup fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ← yahan slash ki jagah dot-slash lagao
+  build: {
+    outDir: "dist", // ensure output folder is dist
+  },
+  base: "", // leave empty (Vercel samajh lega)
 });
