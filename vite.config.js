4fd52700791240e965fad592ede6861ec710b865
fizzajawed85 <angelfizza85@gmail.com>
2025-10-17 21:46:22 +0500

    Final Vercel fix (dynamic routes + MIME)

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
-  base: './', // ✅ important for correct asset loading
+  base: '', // ⚡ leave completely empty (not './' or '/')
 })
-
 
